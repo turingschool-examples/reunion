f6e7336f48add1af4904b23cae2527a05ad83350
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-06 10:23:58 -0700

    Adds passing test and method for breakout. I HAVE NEVER BEEN ABLE TO DO THIS IN A CHALLENGE! =D

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,4 +10,22 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    total_cost = 0
+    @activities.each do |activity|
+      total_cost += activity.total_cost
+    end
+    total_cost
+  end
+
+  def breakout
+    hash = Hash.new(0)
+    @activities.each do |activity|
+      activity.owed.each do |key, value|
+        hash[key] += value
+      end
+    end
+    hash
+  end
 end
