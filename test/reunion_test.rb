7b5cb5e774f86296e11c3b59bbf944acb8d644ab
Tony Robertson <tonyr729@gmail.com>
2018-11-06 09:43:40 -0700

    Add test for reunion properties

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,10 +4,18 @@ require './lib/reunion'
 
 class ReunionTest < Minitest::Test
   def setup
-    @reunion = Reunion.new
+    @reunion = Reunion.new("1406 BE")
   end
 
   def test_it_exists
     assert_instance_of Reunion, @reunion
+  end
+
+  def test_it_has_a_name
+    assert_equal "1406 BE", @reunion.name
+  end
+
+  def test_it_starts_with_no_activities
+    assert_equal [], @reunion.activities
   end
 end
