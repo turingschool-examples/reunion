b21abdbb0d89cb5cf2813bf468b27176dfa4954c
Tony Robertson <tonyr729@gmail.com>
2018-11-06 09:15:59 -0700

    Add tests for properties

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,10 +4,19 @@ require './lib/activity'
 
 class ActivityTest < Minitest::Test
   def setup
-    @activity = Activity.new
+    @activity = Activity.new("Brunch")
   end
 
   def test_it_exists
     assert_instance_of Activity, @activity
+  end
+
+  def test_it_has_a_name
+    assert_equal "Brunch", @activity.name
+  end
+
+  def test_it_starts_with_empty_participants
+    assert_equal 0, @activity.participants.count
+    assert_equal Hash, @activity.participants.class
   end
 end
