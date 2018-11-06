e589106704d00e9c3dd6131b422d583be7adfff1
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-06 09:24:24 -0700

    complete through total cost method

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,9 +1,23 @@
 class Activity
 
-  attr_reader :name
+  attr_reader :name,
+              :participants
 
   def initialize(activity_type)
     @name = activity_type
+    @participants = {}
+  end
+
+  def add_participant(participant, cost)
+    @participants[participant] = cost
+  end
+
+  def total_cost
+    sum = 0
+    @participants.each do |participant, cost|
+      sum += cost
+    end
+    sum
   end
 
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -15,4 +15,23 @@ class ActivityTest < Minitest::Test
     assert_equal "Brunch", activity.name
   end
 
+  def test_it_has_empty_participants_hash
+    activity = Activity.new("Brunch")
+    assert_equal ({}), activity.participants
+  end
+
+  def test_it_can_add_participants
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    actual   = activity.participants
+    expected = {"Maria" => 20}
+    assert_equal expected, actual
+  end
+
+  def test_it_knows_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    assert_equal 20, activity.total_cost
+  end
+
 end
