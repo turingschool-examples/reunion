d3c62855d8401f923a2efb4b8b5f86780aac96de
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-06 09:27:31 -0700

    Adds tests for split and owed

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/activity'
 
 class ActivityTest < Minitest::Test
   def setup
@@ -36,5 +37,23 @@ class ActivityTest < Minitest::Test
     @activity.add_participant("Maria", 20)
 
     assert_equal 20, @activity.total_cost
+  end
+
+  def test_it_returns_split_a_participant_should_pay_given_total_cost
+    @activity.add_participant("Maria", 20)
+    @activity.add_participant("Luther", 40)
+
+    assert_equal 60, @activity.total_cost
+    assert_equal 30, @activity.split
+  end
+
+  def test_it_returns_participants_and_amount_owed
+    @activity.add_participant("Maria", 20)
+    @activity.add_participant("Luther", 40)
+
+    assert_equal 30, @activity.split
+    expected = {"Maria" => 10, "Luther" => -10}
+
+    assert_equal expected, @activity.owed
   end
 end
