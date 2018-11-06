14dbda42b3f575de8047333553a0e55edc43b828
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-06 10:04:55 -0700

    Iteration_3 complete through total cost. Next step reunion.breakout

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -32,7 +32,7 @@ class ReunionTest < Minitest:: Test
   end
 
   def test_it_knows_total_cost_for_one_activity
-    reunion = Reunion.new("1406 BE")
+    reunion    = Reunion.new("1406 BE")
     activity_1 = Activity.new("Brunch")
     activity_1.add_participant("Maria", 20)
     activity_1.add_participant("Luther", 40)
@@ -41,7 +41,26 @@ class ReunionTest < Minitest:: Test
     assert_equal 60, reunion.total_cost
   end
 
-  
+  def test_it_knows_total_cost_for_multiple_activities
+    reunion    = Reunion.new("1406 BE")
+
+    activity_1 = Activity.new("Brunch")
+    activity_2 = Activity.new("Drinks")
+
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+
+    assert_equal 180, reunion.total_cost
+  end
+
+
 
 
 end
