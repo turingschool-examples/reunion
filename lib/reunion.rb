ae37ebe506360ea7a362373dd601c9d0619449f3
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-11-06 11:21:11 -0700

    breakout solution #3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -13,15 +13,21 @@ class Reunion
   end
 
   def total_cost
-    @activities.map do |person|
-        person.total_cost
-    end.flatten.sum
-  end
-
-  def breakout
-    @activities.map do |activity|
-      # binding.pry
-      activity.split
+  #   @activities.map do |person|
+  #       person.total_cost
+  #   end.flatten.sum
+  # end
+  #
+  # def breakout
+  #   array_owed = @activities.map do |activity|
+  #     activity.owed
+  #   end
+  #   array_owed[0].values.inject { |sum, n| sum + n }
+  debt_amt = {}
+    @activities.map.with_index do |activity, i|
+      binding.pry
+      debt_amt[person] = person.owed
     end
+    debt_amt
   end
 end
