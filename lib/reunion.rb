b62d42c0562d916a3c8388c6e90dc1e238e89eed
Asa Holt <holt.ace@icloud.com>
2018-11-06 10:07:10 -0700

    super close to figuring out breakout

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -8,4 +8,19 @@ class Reunion
  def add_activity(activity)
    @activities << activity
  end
+
+ def total_cost
+   @activities.sum do |activity|
+     activity.total_cost
+   end
+ end
+
+ def breakout
+   almost_there = @activities.map do |activity|
+     activity.owed.transform_values do |owe|
+       owe
+     end
+   end
+   almost_there[0].merge(almost_there[1])
+ end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -25,4 +25,35 @@ class ReunionTest < Minitest::Test
     reunion.add_activity(activity_1)
     assert_equal [activity_1], reunion.activities
   end
+
+  def test_total_cost
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    assert_equal 60, reunion.total_cost
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    assert_equal 180, reunion.total_cost
+  end
+
+  def test_breakout
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+    expected = {"Maria" => -10, "Luther" => -30, "Louis" => 40}
+    assert_equal expected, reunion.breakout
+  end
 end
