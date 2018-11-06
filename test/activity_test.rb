9775bae145792b0082c1a5101ddef859efddd55c
abenetka <ali.benetka@gmail.com>
2018-11-06 09:42:54 -0700

    Adds remaining iteration 1 tests and tests for iteration 2

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -23,14 +23,44 @@ class ActivityTest < Minitest::Test
   def test_it_can_add_participants
     activity = Activity.new("Brunch")
     activity.add_participant("Maria", 20)
+
     assert_equal ({"Maria" => 20}), activity.participants
+
+    activity.add_participant("Luther", 40)
+    expected_2 = ({"Maria" => 20, "Luther" => 40})
+
+    assert_equal expected_2, activity.participants
   end
 
   def test_it_can_calculate_total_cost
     activity = Activity.new("Brunch")
     activity.add_participant("Maria", 20)
+
     assert_equal 20, activity.total_cost
+
+    activity.add_participant("Luther", 40)
+
+    assert_equal 60, activity.total_cost
   end
+
+    def test_it_split_total_cost
+      activity = Activity.new("Brunch")
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+
+      assert_equal 30, activity.split
+    end
+
+    def test_it_can_calculate_amount_owed
+      activity = Activity.new("Brunch")
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+
+      assert_equal ({"Maria" => 10, "Luther" => -10}), activity.owed
+    end
+
+
+
 
 
 
