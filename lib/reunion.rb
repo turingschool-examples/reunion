74fdc456e342512e7222e8340bb44e56261ecac2
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-11-06 10:51:37 -0700

    iteration 3 completed, moving onto the hard stuff

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,4 +10,28 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    @activities.sum do |activity|
+      activity.total_cost
+    end
+  end
+
+  def breakout
+    breakdown = Hash.new(0)
+    @activities.each do |activity|
+      breakup = activity.owed
+      breakup.each do |participant|
+        breakdown[participant[0]] += participant[1]
+      end
+    end
+    breakdown
+  end
+
+  def summary
+    look_nice = breakout.map do |key, value|
+      "#{key}: #{value}\n"
+    end
+    look_nice.join
+  end
 end
