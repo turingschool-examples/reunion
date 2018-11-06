ad22baffc2ee7ec36cc72f61fe62a2258f69182f
Tony Robertson <tonyr729@gmail.com>
2018-11-06 10:14:09 -0700

    Add method to satisfy test for breakout test

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -13,4 +13,14 @@ class Reunion
   def total_cost
     @activities.inject(0) {|total_cost, activity| total_cost + activity.total_cost}
   end
+
+  def breakout
+    @activities.each_with_index.inject({}) do |total, (activity, index)|
+      next_act = @activities[index + 1]
+      if next_act
+        total = activity.owed.merge(next_act.owed) {|key, val1, val2| val1 + val2}
+      end
+      total
+    end
+  end
 end
