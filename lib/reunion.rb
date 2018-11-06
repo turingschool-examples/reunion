f181fe6e1d18347b22f729c289719ba55aff4877
Ben Lee <bendelonlee@gmail.com>
2018-11-06 09:47:19 -0700

    Through iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -9,5 +9,23 @@ class Reunion
     @activities << activity
   end
 
+  def total_cost
+    @activities.sum{|activity| activity.total_cost}
+  end
+
+  def breakout
+    @activities.reduce(Hash.new(0)) do |rslt, activity|
+      rslt.merge(activity.owed) do |key, oldval, newval|
+        oldval + newval
+      end
+    end
+  end
+
+  def summary
+    breakout.reduce([]) do |rslt, (name, owed)|
+      rslt << "#{name}: #{owed}"
+    end.join("\n")
+  end
+
 
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -6,6 +6,18 @@ class ReunionTest < Minitest::Test
     @reunion = Reunion.new("1406 BE")
   end
 
+  def setup_two_activities
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    @reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    @reunion.add_activity(activity_2)
+  end
+
   def test_it_exists
     assert_instance_of Reunion, @reunion
   end
@@ -31,5 +43,57 @@ class ReunionTest < Minitest::Test
     @reunion.add_activity(activity_2)
     assert_equal [activity_1, activity_2], @reunion.activities
   end
+
+  def test_total_cost_sums_all_costs_of_all_activities
+    setup_two_activities
+    assert_equal 180, @reunion.total_cost
+  end
+
+  def test_breakout_breaks_out_what_is_owed_by_each_participant
+    setup_two_activities
+    assert_equal ({"Maria" => -10, "Luther" => -30, "Louis" => 40}), @reunion.breakout
+  end
+
+  def test_summary_returns_breakout_as_a_string
+    setup_two_activities
+    assert_equal "Maria: -10\nLuther: -30\nLouis: 40", @reunion.summary
+  end
+
+
+
+# reunion = Reunion.new("1406 BE")
+# => #<Reunion:0x007fe4ca1defc8 ...>
+
+# activity_1 = Activity.new("Brunch")
+
+# activity_1.add_participant("Maria", 20)
+
+# activity_1.add_participant("Luther", 40)
+
+# reunion.add_activity(activity_1)
+
+# reunion.total_cost
+# => 60
+
+# activity_2 = Activity.new("Drinks")
+
+# activity_2.add_participant("Maria", 60)
+
+# activity_2.add_participant("Luther", 60)
+
+# activity_2.add_participant("Louis", 0)
+
+# reunion.add_activity(activity_2)
+
+# reunion.total_cost
+# => 180
+
+# reunion.breakout
+# => {"Maria" => -10, "Luther" => -30, "Louis" => 40}
+
+# reunion.summary
+# => "Maria: -10\nLuther: -30\nLouis: 40"
+
+# puts reunion.summary
 
 end
