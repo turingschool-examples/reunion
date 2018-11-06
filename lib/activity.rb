9becfdc2889f873c39b6fc11f52f0d672423c4f9
abenetka <ali.benetka@gmail.com>
2018-11-06 09:43:14 -0700

    Adds implementation through iteration 2

--- lib/activity.rb
+++ lib/activity.rb
@@ -6,12 +6,25 @@ class Activity
     @participants = {}
   end
 
-  def add_participant(person, amount)
-    @participants[person] = amount
+  def add_participant(person, amount_paid)
+    @participants[person] = amount_paid
   end
 
   def total_cost
     @participants.values.sum
+  end
+
+  def split
+    number_of_participants = @participants.keys.length
+    total_cost / number_of_participants
+  end
+
+  def owed
+    amount_owed = Hash.new(0)
+    @participants.each do |person, amount_paid|
+      amount_owed[person] = split - amount_paid
+    end
+    amount_owed
   end
 
 
