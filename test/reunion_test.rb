f05b3b1a62058aaee9104dbf6f06b0050145681c
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-11-06 09:49:29 -0700

    didn't see other part, now completed iteration 1 & 2

---
+++ test/reunion_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/reunion'
+require './lib/activity'
+
+class ReunionTest < Minitest::Test
+  def test_it_exists
+    reunion = Reunion.new("1406 BE")
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_it_has_attributes
+    reunion = Reunion.new("1406 BE")
+    assert_equal '1406 BE', reunion.name
+  end
+
+  def test_activities_starts_with_empty_array
+    reunion = Reunion.new("1406 BE")
+    assert_equal [], reunion.activities
+  end
+
+  def test_case_name
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    reunion.add_activity(activity_1)
+    assert_equal [activity_1], reunion.activities
+  end
+end
