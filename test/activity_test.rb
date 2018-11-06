e20993d2925c84455a811d1450ed0e8d9dec2ea9
Maddie Jones <maddyg91@gmail.com>
2018-11-06 09:35:17 -0700

    tests for split and owed added, first few tests for reunion class written

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -35,7 +35,25 @@ class ActivityTest < Minitest::Test
   def test_it_can_calculate_total_cost
     activity = Activity.new("Brunch")
     activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
 
-    assert_equal 20, activity.total_cost
+    assert_equal 60, activity.total_cost
+  end
+
+  def test_it_can_split_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    assert_equal 30, activity.split
+  end
+
+  def test_it_can_calculate_what_is_owed
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expected = {"Maria" => 10, "Luther" => -10}
+    assert_equal expected, activity.owed
   end
 end
