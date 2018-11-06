4e2710b35ded9b1e76677f0ad2784a941b266bf0
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-06 11:58:57 -0700

    Adds test for detailed_breakout method in Iteration 4

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -71,5 +71,46 @@ class ReunionTest < Minitest::Test
     assert_equal expected, reunion.summary
   end
 
+  def test_it_returns_detailed_breakout
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    activity_3 = Activity.new("Bowling")
+    activity_3.add_participant("Maria", 0)
+    activity_3.add_participant("Luther", 0)
+    activity_3.add_participant("Louis", 30)
+    activity_4 = Activity.new("Jet Skiing")
+    activity_4.add_participant("Maria", 0)
+    activity_4.add_participant("Luther", 0)
+    activity_4.add_participant("Louis", 40)
+    activity_4.add_participant("Nemo", 40)
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+    reunion.add_activity(activity_3)
+    reunion.add_activity(activity_4)
+    expected = {
+  "Maria" => [{ activity: "Brunch", payees: ["Luther"], amount: 10},
+    { activity: "Drinks", payees: ["Louis"], amount: -20},
+    { activity: "Bowling", payees: ["Louis"], amount: 10},
+    { activity: "Jet Skiing", payees: ["Louis", "Nemo"], amount: 10}],
+  "Luther" => [
+    { activity: "Brunch", payees: ["Maria"], amount: -10},
+    { activity: "Drinks", payees: ["Louis"], amount: -20},
+    { activity: "Bowling", payees: ["Louis"], amount: 10},
+    { activity: "Jet Skiing", payees: ["Louis", "Nemo"], amount: 10}],
+  "Louis" => [
+    { activity: "Drinks", payees: ["Maria", "Luther"], amount: 20},
+    { activity: "Bowling", payees: ["Maria", "Luther"], amount: -10},
+    { activity: "Jet Skiing", payees: ["Maria", "Luther"], amount: -10}],
+  "Nemo" => [
+    { activity: "Jet Skiing", payees: ["Maria", "Luther"], amount: -10}]
+  }
+    assert_equal expected, reunion.detailed_breakout
+  end
 
 end
