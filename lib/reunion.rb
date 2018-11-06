fd6beb4ed5fbcf62898f9ec7533468fbc2a30dd4
Maddie Jones <maddyg91@gmail.com>
2018-11-06 09:58:03 -0700

    all test for iteration 2 passing

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,21 +1,14 @@
-# ```ruby
-# pry(main)> require './lib/reunion'
-# # => true
-#
-# pry(main)> reunion = Reunion.new("1406 BE")
-# # => #<Reunion:0x007fe4ca1defc8 ...>
-#
-# pry(main)> reunion.name
-# # => "1406 BE"
-#
-# pry(main)> reunion.activities
-# # => []
-#
-# pry(main)> activity_1 = Activity.new("Brunch")
-# # => #<Activity:0x007fe4ca1d9438 ...>
-#
-# pry(main)> reunion.add_activity(activity_1)
-#
-# pry(main)> reunion.activities
-# # => [#<Activity:0x007fe4ca1d9438 ...>]
-# ```
+require 'pry'
+class Reunion
+  attr_reader :name, :activities
+
+  def initialize(name)
+    @name = name
+    @activities = []
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
+  
+end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/reunion'
+require './lib/activity'
 require 'pry'
 
 class ReunionTest < Minitest::Test
@@ -14,5 +15,19 @@ class ReunionTest < Minitest::Test
     reunion = Reunion.new("1406 BE")
 
     assert_equal "1406 BE", reunion.name
+  end
+
+  def test_it_can_store_activities
+    reunion = Reunion.new("1406 BE")
+
+    assert_equal [], reunion.activities
+  end
+
+  def test_it_can_add_activities
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    reunion.add_activity(activity_1)
+
+    assert_equal [activity_1], reunion.activities
   end
 end
