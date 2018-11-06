af994cf594966f54ceaa56222ab1bf4df842a72e
Rene Casco <rene.casco@icloud.com>
2018-11-06 09:27:38 -0700

    Iteration 1 completed

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -10,4 +10,29 @@ class ActivityTest < Minitest::Test
     assert_instance_of Activity, activity
   end
 
+  def test_it_has_name
+    activity = Activity.new("Brunch")
+    assert_equal "Brunch", activity.name
+  end
+
+  def test_participants_is_empty_hash_by_default
+    activity = Activity.new("Brunch")
+    assert_equal ({}), activity.participants
+  end
+
+  def test_it_can_add_participants
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+    assert_equal ({"Maria" => 20, "Luther" => 40}), activity.participants
+  end
+
+  def test_it_can_get_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    assert_equal 20, activity.total_cost
+    activity.add_participant("Luther", 40)
+    assert_equal 60, activity.total_cost
+  end
+
 end
