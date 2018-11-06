a0e6a0a90fc991ec0cbd403879af4c94e4752cd2
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-06 10:06:49 -0700

    adds total_cost, breakout, and summary

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -8,4 +8,27 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    activities.reduce(0) do |total, activity|
+      total += activity.total_cost
+    end
+  end
+
+  def breakout
+    activities.reduce(Hash.new(0)) do |breakout, activity|
+      activity.owed.each do |participant, owed|
+        breakout[participant] += owed
+      end
+      breakout
+    end
+  end
+
+  def summary
+    breakout.map.with_index do |(participant, owed), index|
+      summary = "#{participant}: #{owed}"
+      summary += "\n" unless index == breakout.length - 1
+      summary
+    end.join
+  end
 end
