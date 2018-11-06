9d66c9e9a1b3754012dd465c0468d4b27c922944
Anten Skrabec <cebarks@gmail.com>
2018-11-06 09:45:24 -0700

    updates ReunionTest to iteration 3

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -24,6 +24,41 @@ class ReunionTest < Minitest::Test
   def test_it_can_add_an_activity
     activity_1 = Activity.new("Brunch")
     @reunion.add_activity(activity_1)
+
     assert_equal [activity_1], @reunion.activities
+  end
+
+  def test_it_can_calculate_total_cost
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    @reunion.add_activity(activity_1)
+
+    assert_equal 60, @reunion.total_cost
+  end
+
+  def test_it_can_breakout
+    activity_1 = Activity.new("Brunch")
+
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    @reunion.add_activity(activity_1)
+
+    activity_2 = Activity.new("Drinks")
+
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    @reunion.add_activity(activity_2)
+
+    assert_equal 180, @reunion.total_cost
+    assert_equal ({ "Maria" => -10, "Luther" => -30, "Louis" => 40 }), @reunion.breakout
+  end
+
+  def test_it_can_generate_a_summary
+    assert_equal "Maria: -10\nLuther: -30\nLouis: 40", @reunion.summary
   end
 end
