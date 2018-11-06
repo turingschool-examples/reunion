e5f1b4252c16fedb0b1529b317ac2aca991de02a
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-06 09:27:30 -0700

    Completion of test suite for iteration 2

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
 
@@ -12,6 +13,18 @@ class ReunionTest < Minitest::Test
   def test_it_has_name
     reunion = Reunion.new("1406 BE")
     assert_equal "1406 BE", reunion.name
+  end
+
+  def test_activities_start_as_empty_array
+    reunion = Reunion.new("1406 BE")
+    assert_equal [], reunion.activities
+  end
+
+  def test_it_can_add_and_hold_activities
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    reunion.add_activity(activity_1)
+    assert_equal [activity_1], reunion.activities
   end
 
 
