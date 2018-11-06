f629cfbd7f55ca0697e9bb8bf3bf935a2b013ed1
abenetka <ali.benetka@gmail.com>
2018-11-06 10:21:13 -0700

    Adds tests for iteration 3

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -28,6 +28,70 @@ class ReunionTest < Minitest::Test
     assert_equal [activity_1], reunion.activities
   end
 
+  def test_it_has_a_total_cost
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_2 = Activity.new("Drinks")
+
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    reunion.add_activity(activity_1)
+
+    assert_equal 60, reunion.total_cost
+
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    reunion.add_activity(activity_2)
+
+    assert_equal 180, reunion.total_cost
+  end
+
+  def test_it_can_calculate_breakout
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_2 = Activity.new("Drinks")
+
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+
+    expected = ({"Maria" => -10, "Luther" => -30, "Louis" => 40})
+
+    assert_equal expected, reunion.breakout
+  end
+
+  def test_it_print_a_summary
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_2 = Activity.new("Drinks")
+
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+
+    expected = "Maria: -10\nLuther: -30\nLouis: 40"
+
+    assert_equal expected, reunion.summary
+  end
+
+
+
+
 
 
 
