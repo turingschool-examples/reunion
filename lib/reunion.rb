bee1a701415d1bd84e79f7665b478e31a6032647
Lance Taylor <lance.taylor98@hotmail.com>
2018-11-06 11:14:18 -0700

    working on getting the breakout to work correctly

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -15,4 +15,15 @@ class Reunion
       (activity.total_cost)
     end.sum
   end
+
+  def breakout
+    require 'pry';binding.pry
+    hash_array =
+    @activities.map do |activity|
+      activity.owed
+    end
+    hash_array.inject({}) do |sum, activity_total|
+      sum.merge(activity_total)
+    end
+  end
 end
