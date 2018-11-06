94f674336093bb3c38dbf8f2fcee860f57e494e7
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-06 09:52:45 -0700

    Adds Iteration 3 breakout and summary

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -11,4 +11,26 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+  
+  def total_cost
+    @activities.sum do |activity|
+      activity.total_cost
+    end
+  end
+  
+  def breakout
+    breakout = Hash.new(0)
+    @activities.each do |activity|
+      activity.owed.each do |name, owed|
+        breakout[name] += owed
+      end
+    end
+    breakout
+  end
+  
+  def summary
+    breakout.map do |name, owed|
+      "#{name}: #{owed}"
+    end.join("\n")
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -27,13 +27,60 @@ class ReunionTest < Minitest::Test
     assert_equal [activity_1], reunion.activities
   end
   
+  def test_it_can_calculate_total_cost
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    assert_equal 60, reunion.total_cost
+    
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    assert_equal 180, reunion.total_cost
+  end
+  
+  def test_it_can_return_breakout_of_what_is_owed
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    expected = ({"Maria" => -10, "Luther" => -30, "Louis" => 40})
+    assert_equal expected, reunion.breakout
+  end
+  
+  def test_it_can_return_printable_summary_of_breakout
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    expected = "Maria: -10\nLuther: -30\nLouis: 40"
+    assert_equal expected, reunion.summary
+  end
 end
 
+# pry(main)> reunion.breakout
+# # => {"Maria" => -10, "Luther" => -30, "Louis" => 40}
 # 
-# pry(main)> activity_1 = Activity.new("Brunch")
-# # => #<Activity:0x007fe4ca1d9438 ...>
+# pry(main)> reunion.summary
+# # => "Maria: -10\nLuther: -30\nLouis: 40"
 # 
-# pry(main)> reunion.add_activity(activity_1)
-# 
-# pry(main)> reunion.activities
-# # => [#<Activity:0x007fe4ca1d9438 ...>]
+# pry(main)> puts reunion.summary
+# Maria: -10
+# Luther: -30
+# Louis: 40
