61cce4bb462b83dfe37d4041d4f86f2f39e9c2e9
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-06 09:57:51 -0700

    Adds test suite for completion of iteration 3

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -27,7 +27,49 @@ class ReunionTest < Minitest::Test
     assert_equal [activity_1], reunion.activities
   end
 
+  def test_it_can_track_total_cost_of_all_activities
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    assert_equal 60, reunion.total_cost
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    assert_equal 180, reunion.total_cost
+  end
 
+  def test_it_can_track_breakout
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    assert_equal ({"Maria" => -10, "Luther" => -30, "Louis" => 40}), reunion.breakout
+  end
+
+  def test_it_can_return_summary
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    expected = "Maria: -10\nLuther: -30\nLouis: 40"
+    assert_equal expected, reunion.summary
+  end
 
 
 end
