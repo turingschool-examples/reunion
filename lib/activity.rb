1f5950b1faa707d538ee92f73b27a266bfa75446
Leah Miller <lkmiller.87@gmail.com>
2018-11-06 09:08:32 -0700

    Add initial setup for activity

---
+++ lib/activity.rb
@@ -0,0 +1,6 @@
+class Activity
+  attr_reader :name 
+  def initialize(name)
+    @name = name
+  end
+end
---
+++ test/activity_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/activity'
+
+class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("Brunch")
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new("Brunch")
+    assert_equal "Brunch", activity.name
+  end
+end
