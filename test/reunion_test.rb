4f0a4cf9c104a572068765f262b5a7ed549df20d
Brad Niedt <bradleyniedt@gmail.com>
2018-11-06 09:52:47 -0700

    adds test for summary method

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -52,6 +52,21 @@ class ReunionTest < Minitest::Test
     activity_2.add_participant("Louis", 0)
     reunion.add_activity(activity_2)
     expected = {"Maria" => -10, "Luther" => -30, "Louis" => 40}
-    assert_equal expected, reunion.breakout 
+    assert_equal expected, reunion.breakout
+  end
+
+  def test_it_can_return_a_summary
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    expected = "Maria: -10\nLuther: -30\nLouis: 40"
+    assert_equal expected, reunion.summary
   end
 end
