a0933fc78f3d3a6ea0473e8709e4a7b06f8d0001
Brian Zanti <brianzanti@gmail.com>
2018-11-05 22:53:49 -0700

    removes starter files

--- test/activity_test.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/activity'
-
-class ActivityTest < Minitest::Test
-end
--- test/reunion_test.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest'
-require 'minitest/test'
-require 'minitest/autorun'
-require './lib/reunion'
-
-class ReunionTest < Minitest::Test
-end
