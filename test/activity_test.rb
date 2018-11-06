20e233f4ebd5cec4ad631427b6daaff0af87a33c
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-06 09:13:38 -0700

    Adds test suite for iteration 1

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -14,6 +14,28 @@ class ActivityTest < Minitest::Test
     assert_equal ({}), activity.participants
   end
 
+  def test_it_can_add_participants
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    assert_equal ({"Maria" => 20}), activity.participants
+  end
+
+  def test_it_tracks_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    assert_equal 20, activity.total_cost
+    activity.add_participant("Steven", 60)
+    assert_equal 80, activity.total_cost
+  end
+
+  def test_it_can_track_multiple_people
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    assert_equal ({"Maria" => 20}), activity.participants
+    activity.add_participant("Luther", 40)
+    assert_equal ({"Maria" => 20, "Luther" => 40}), activity.participants
+  end
+
 
 
 end
