45190bb4a5c7c2a8dfd5f723c60884bdf1ee0e01
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-06 10:58:19 -0700

    Adds test for detailed_breakdown

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -32,7 +32,7 @@ class ReunionTest < Minitest::Test
     activity_2.add_participant("Louis", 0)
     @reunion.add_activity(activity_2)
 
-    assert_equal 180, @reunion.total_cost
+    assert_equal 120, @reunion.total_cost
   end
 
   def test_it_returns_a_breakout_and_summary_of_final_amount_owed_for_each_participant
@@ -55,5 +55,104 @@ class ReunionTest < Minitest::Test
 
     assert_equal expected, @reunion.breakout
     assert_equal expected_summary, @reunion.summary
+  end
+
+  def test_it_returns_detailed_breakdown
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+
+    @activity_2 = Activity.new("Drinks")
+    @activity_2.add_participant("Maria", 60)
+    @activity_2.add_participant("Luther", 60)
+    @activity_2.add_participant("Louis", 0)
+
+    @activity_3 = Activity.new("Bowling")
+    @activity_3.add_participant("Maria", 0)
+    @activity_3.add_participant("Luther", 0)
+    @activity_3.add_participant("Louis", 30)
+
+    @activity_4 = Activity.new("Jet Skiing")
+    @activity_4.add_participant("Maria", 0)
+    @activity_4.add_participant("Luther", 0)
+    @activity_4.add_participant("Louis", 40)
+    @activity_4.add_participant("Nemo", 40)
+
+    @reunion.add_activity(@activity_1)
+    @reunion.add_activity(@activity_2)
+    @reunion.add_activity(@activity_3)
+    @reunion.add_activity(@activity_4)
+
+    expected = {
+      "Maria" => [
+        {
+          activity: "Brunch",
+          payees: ["Luther"],
+          amount: 10
+        },
+        {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Luther" => [
+        {
+          activity: "Brunch",
+          payees: ["Maria"],
+          amount: -10
+        },
+        {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Louis" => [
+        {
+          activity: "Drinks",
+          payees: ["Maria", "Luther"],
+          amount: 20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ],
+      "Nemo" => [
+        {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ]
+    }
+
+    assert_equal expected, @reunion.detailed_breakdown
   end
 end
