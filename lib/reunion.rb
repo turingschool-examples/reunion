a3b8af76bc4504d3c6c3d1886018daf1bed29105
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-06 11:41:49 -0700

    Adds detailed_breakdown and initial_participants helper

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -31,4 +31,35 @@ class Reunion
       summary
     end.join
   end
+
+  def detailed_breakdown
+    activities.reduce(initial_participants) do |breakdown, activity|
+      bill = activity.owed
+
+      bill.each do |participant, owed|
+        current_is_owed = owed < 0
+        payees = bill.find_all do |person, amount|
+          (amount < 0) != current_is_owed
+        end.to_h.keys
+        activity_breakdown_for_current = {
+          activity: activity.name,
+          payees: payees,
+          amount: owed / payees.length
+        }
+        breakdown[participant] << activity_breakdown_for_current
+      end
+      breakdown
+    end
+  end
+
+  def initial_participants
+    names = activities.map do |activity|
+      activity.participants.keys
+    end.flatten.uniq
+
+    names.reduce({}) do |participants, name|
+      participants[name] = []
+      participants
+    end
+  end
 end
