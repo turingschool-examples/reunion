d91026be14d04e70c88d0a698e6d810c96cd4c19
Tony Robertson <tonyr729@gmail.com>
2018-11-06 11:00:22 -0700

    WIP: Laying out tests for detailed_breakout method

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -76,4 +76,186 @@ class ReunionTest < Minitest::Test
     expected = "Maria: -10\nLuther: -30\nLouis: 40"
     assert_equal expected, @reunion.summary
   end
+
+  def test_it_gives_detailed_breakout
+    skip
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    activity_3 = Activity.new("Bowling")
+    activity_3.add_participant("Maria", 0)
+    activity_3.add_participant("Luther", 0)
+    activity_3.add_participant("Louis", 30)
+
+    activity_4 = Activity.new("Jet Skiing")
+    activity_4.add_participant("Maria", 0)
+    activity_4.add_participant("Luther", 0)
+    activity_4.add_participant("Louis", 40)
+    activity_4.add_participant("Nemo", 40)
+
+    @reunion.add_activity(activity_1)
+    @reunion.add_activity(activity_2)
+    @reunion.add_activity(activity_3)
+    @reunion.add_activity(activity_4)
+
+    expected = {
+      "Maria" => [
+        {
+          activity: "Brunch",
+          payees: ["Luther"],
+          amount: 10
+        },
+        {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Luther" => [
+        {
+          activity: "Brunch",
+          payees: ["Maria"],
+          amount: -10
+        },
+        {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Louis" => [
+        {
+          activity: "Drinks",
+          payees: ["Maria", "Luther"],
+          amount: 20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ],
+      "Nemo" => [
+        {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ]
+    }
+    require "pry"; binding.pry
+    assert_equal expected, @reunion.detailed_breakout
+  end
+
+  def test_it_can_return_a_list_of_participants
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    activity_3 = Activity.new("Bowling")
+    activity_3.add_participant("Maria", 0)
+    activity_3.add_participant("Luther", 0)
+    activity_3.add_participant("Louis", 30)
+
+    activity_4 = Activity.new("Jet Skiing")
+    activity_4.add_participant("Maria", 0)
+    activity_4.add_participant("Luther", 0)
+    activity_4.add_participant("Louis", 40)
+    activity_4.add_participant("Nemo", 40)
+
+    @reunion.add_activity(activity_1)
+    @reunion.add_activity(activity_2)
+    @reunion.add_activity(activity_3)
+    @reunion.add_activity(activity_4)
+
+    assert_equal [], @reunion.participants
+  end
+
+  def test_it_can_list_detailed_info
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    activity_3 = Activity.new("Bowling")
+    activity_3.add_participant("Maria", 0)
+    activity_3.add_participant("Luther", 0)
+    activity_3.add_participant("Louis", 30)
+
+    activity_4 = Activity.new("Jet Skiing")
+    activity_4.add_participant("Maria", 0)
+    activity_4.add_participant("Luther", 0)
+    activity_4.add_participant("Louis", 40)
+    activity_4.add_participant("Nemo", 40)
+
+    @reunion.add_activity(activity_1)
+    @reunion.add_activity(activity_2)
+    @reunion.add_activity(activity_3)
+    @reunion.add_activity(activity_4)
+
+    expected = [
+      {
+        activity: "Brunch",
+        payees: ["Luther"],
+        amount: 10
+      },
+      {
+        activity: "Drinks",
+        payees: ["Louis"],
+        amount: -20
+      },
+      {
+        activity: "Bowling",
+        payees: ["Louis"],
+        amount: 10
+      },
+      {
+        activity: "Jet Skiing",
+        payees: ["Louis", "Nemo"],
+        amount: 10
+      }
+    ]
+
+    assert_equal expected_
+  end
 end
