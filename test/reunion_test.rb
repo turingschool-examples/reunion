b42e4357274123b389b12f2416d3bbf8489fe5fb
Aizar <aizar.cabrera@icloud.com>
2018-11-06 11:16:42 -0700

    adds test_it_can_add_more_activities_and_participants_to_reunion_and_get_the_cost passing

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -27,7 +27,7 @@ class ReunionTest < Minitest::Test
     assert_equal [activity_1], reunion.activities
   end
 
-  def test_it_can_add_participants_to_the_activities_of_reunion
+  def test_it_can_add_participants_to_the_activities_of_reunion_and_get_cost
     reunion = Reunion.new("1406 BE")
     activity_1 = Activity.new("Brunch")
     activity_1.add_participant("Maria", 20)
@@ -36,5 +36,21 @@ class ReunionTest < Minitest::Test
 
     assert_equal 60, reunion.total_cost
   end
+
+  def test_it_can_add_more_activities_and_participants_to_reunion_and_get_the_cost
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_2 = Activity.new("Drinks")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+
+    assert_equal 180, reunion.total_cost
+  end
+
 
 end
