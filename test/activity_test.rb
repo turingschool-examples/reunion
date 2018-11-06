5a742cb3486bbed28c18d550a66801c6350bcb10
Nico <nicovigil1@gmail.com>
2018-11-06 09:36:14 -0700

    adds passing test for multiple participants in activity class

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -31,6 +31,13 @@ class ActivityTest < Minitest::Test
     assert_equal 20, @activity.total_cost
   end
 
+  def test_it_can_add_multiple_participants
+    @activity.add_participant("Maria", 20)
+    @activity.add_participant("Luther", 40)
+
+    assert_equal ({"Maria" => 20, "Luther" => 40}), @activity.participants
+  end
+
   def test_it_can_split_total_cost
     skip
     #it can evenly distribute cost across all participants
