17ed26a099cb0bae53ad8d20b6a71cffff12cb4b
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-06 09:58:00 -0700

    Adds methods for completiong of iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -11,5 +11,29 @@ class Reunion
     @activities << activity
   end
 
+  def total_cost
+    @activities.sum do |activity|
+      activity.total_cost
+    end
+  end
+
+  def breakout
+    final_output = {}
+    @activities.each do |activity|
+      activity.owed.each do |name, money_spent|
+        it_exists = final_output.include?(name)
+        it_exists ? final_output[name] += money_spent : final_output[name] = money_spent
+      end
+    end
+    final_output
+  end
+
+  def summary
+    summary_output = ""
+    breakout.each do |name, balance|
+      summary_output << "#{name}: #{balance}\n"
+    end
+    summary_output.chomp
+  end
 
 end
