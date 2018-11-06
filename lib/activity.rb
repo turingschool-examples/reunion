c68f7a9ec7860d389bf7c3b5f8068d77ee9bb3d7
Anten Skrabec <cebarks@gmail.com>
2018-11-06 09:19:37 -0700

    implements Activity

---
+++ lib/activity.rb
@@ -0,0 +1,18 @@
+class Activity
+  attr_reader :name, :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, cost)
+    @participants[name] = cost
+  end
+
+  def total_cost
+    @participants.sum do |k, v|
+      v
+    end
+  end
+end
