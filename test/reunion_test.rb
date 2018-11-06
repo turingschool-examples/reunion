cb55c17835738dc4931961e7227a76ce3cfeec40
Brad Niedt <bradleyniedt@gmail.com>
2018-11-06 09:41:57 -0700

    adds test for reunion total cost

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -25,4 +25,18 @@ class ReunionTest < Minitest::Test
     reunion.add_activity(activity_1)
     assert_equal [activity_1], reunion.activities
   end
+
+  def test_it_can_get_total_cost_for_the_reunion
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+    assert_equal 180, reunion.total_cost
+  end
 end
