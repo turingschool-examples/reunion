ed44bef1c68c005d9cda0f04251bc8241c5829db
Daniel Briechle <danbriechle@hotmail.com>
2018-11-06 10:03:52 -0700

    created it 3 reunion tests

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -31,21 +31,108 @@ class ReunionTest < Minitest::Test
     assert_equal [activity_1], reunion.activities
   end
 
+  def test_it_can_return_the_total_cost_for_the_whole_reunion_with_just_one_activity
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+
+    assert_equal 60, reunion.total_cost
+  end
+
+  def test_it_can_return_the_total_cost_for_the_whole_reunion_with_more_than_one_activity
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    assert_equal 180, reunion.total_cost
+  end
+
+  def test_it_can_return_a_breakout_hash_of_what_each_person_owes_for_the_whole_reunion
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    expected = {"Maria" => -10, "Luther" => -30, "Louis" => 40}
+    assert_equal expected, reunion.breakout
+  end
+
+  def test_it_can_produce_string_sumary_of_the_breakout_properly_formated
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    expected = "Maria: -10\nLuther: -30\nLouis: 40"
+    assert_equal expected, reunion.breakout
+
+  end
+
 end
 
-# reunion = Reunion.new("1406 BE")
-# # => #<Reunion:0x007fe4ca1defc8 ...>
+
+
+# # breakout` method, the key is a person's name and the value is what they owe for the whole reunion
 #
-# reunion.name
-# # => "1406 BE"
+#  reunion = Reunion.new("1406 BE")
+# #
+#  activity_1 = Activity.new("Brunch")
 #
-# reunion.activities
-# # => []
+#  activity_1.add_participant("Maria", 20)
 #
-# activity_1 = Activity.new("Brunch")
-# # => #<Activity:0x007fe4ca1d9438 ...>
+#  activity_1.add_participant("Luther", 40)
 #
-# reunion.add_activity(activity_1)
+#  reunion.add_activity(activity_1)
 #
-# reunion.activities
-# # => [#<Activity:0x007fe4ca1d9438 ...>]
+#  reunion.total_cost
+# # => 60
+#
+#  activity_2 = Activity.new("Drinks")
+#
+#  activity_2.add_participant("Maria", 60)
+#
+#  activity_2.add_participant("Luther", 60)
+#
+#  activity_2.add_participant("Louis", 0)
+#
+#  reunion.add_activity(activity_2)
+#
+#  reunion.total_cost
+# # => 180
+#
+#  reunion.breakout
+# # => {"Maria" => -10, "Luther" => -30, "Louis" => 40}
+#
+#  reunion.summary
+# # => "Maria: -10\nLuther: -30\nLouis: 40"
+#
+#  puts reunion.summary
+# Maria: -10
+# Luther: -30
+# Louis: 40
+# ```
+#
