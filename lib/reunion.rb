2dbe67cbfec3f80a094eb019ce376625568354b1
abenetka <ali.benetka@gmail.com>
2018-11-06 10:21:32 -0700

    Adds implementation of iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,5 +10,32 @@ class Reunion
     @activities << activity
   end
 
+  def total_cost
+    @activities.map do |activity|
+       activity.total_cost
+      # binding.pry
+    end.sum
+  end
+
+  def breakout
+    breakout_hash = Hash.new(0)
+    @activities.each do |activity|
+      activity.owed.map do |person, amount|
+        breakout_hash[person] += amount
+      end
+    end
+    breakout_hash
+  end
+
+  def summary
+    breakout.map do |person, amount|
+      "#{person}: #{amount}"
+    end.join("\n")
+  end
+
+
+
+
+
 
 end
