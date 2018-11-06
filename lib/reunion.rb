6580758a63dd8edea9971cb6d4daa4db75b4ebcb
Mary Goodhart <marya.grace5@gmail.com>
2018-11-06 10:10:27 -0700

    it 2 passing tests and methods

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,10 +1,15 @@
 require 'pry'
 
 class Reunion
-    attr_reader :name
+    attr_reader :name, :activities
 
   def initialize(name)
     @name = name
+    @activities = []
   end
-  
+
+  def add_activity(activity)
+    @activities << activity
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -17,4 +17,14 @@ class ReunionTest < Minitest::Test
 
     assert_equal [], reunion.activities
   end
+
+  def test_that_it_can_add_activities
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    reunion.add_activity(activity_1)
+
+    assert_equal 1, reunion.activities.length
+    binding.pry
+    assert_instance_of Activity, reunion.activities[0]
+  end
 end
