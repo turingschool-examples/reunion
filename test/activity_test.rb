4cf58ec04b70c0fef70edc4b69245c28d81ddac0
abenetka <ali.benetka@gmail.com>
2018-11-06 10:40:32 -0700

    Refactors tests in iteration 1 to better follow interaction pattern

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -3,7 +3,6 @@ require 'minitest/pride'
 require './lib/activity'
 require 'pry'
 
-
 class ActivityTest < Minitest::Test
   def test_it_exists
     activity = Activity.new("Brunch")
@@ -20,49 +19,55 @@ class ActivityTest < Minitest::Test
     assert_equal ({}), activity.participants
   end
 
-  def test_it_can_add_participants
+  def test_it_can_add_a_participant
     activity = Activity.new("Brunch")
     activity.add_participant("Maria", 20)
 
-    assert_equal ({"Maria" => 20}), activity.participants
-
-    activity.add_participant("Luther", 40)
-    expected_2 = ({"Maria" => 20, "Luther" => 40})
-
-    assert_equal expected_2, activity.participants
+    expected = ({"Maria" => 20})
+    assert_equal expected, activity.participants
   end
 
-  def test_it_can_calculate_total_cost
+  def test_it_can_multiple_participants
+    activity = Activity.new("Brunch")
+
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expected = ({"Maria" => 20, "Luther" => 40})
+    assert_equal expected, activity.participants
+  end
+
+  def test_it_can_calculate_total_cost_with_one_participant
     activity = Activity.new("Brunch")
     activity.add_participant("Maria", 20)
 
     assert_equal 20, activity.total_cost
+  end
 
+  def test_it_can_calculate_total_cost_with_multiple_participants
+    activity = Activity.new("Brunch")
+
+    activity.add_participant("Maria", 20)
     activity.add_participant("Luther", 40)
 
     assert_equal 60, activity.total_cost
   end
 
-    def test_it_split_total_cost
-      activity = Activity.new("Brunch")
-      activity.add_participant("Maria", 20)
-      activity.add_participant("Luther", 40)
+  def test_it_split_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
 
-      assert_equal 30, activity.split
-    end
+    assert_equal 30, activity.split
+  end
 
-    def test_it_can_calculate_amount_owed
-      activity = Activity.new("Brunch")
-      activity.add_participant("Maria", 20)
-      activity.add_participant("Luther", 40)
+  def test_it_can_calculate_amount_owed
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
 
-      assert_equal ({"Maria" => 10, "Luther" => -10}), activity.owed
-    end
-
-
-
-
-
-
+    expected = ({"Maria" => 10, "Luther" => -10})
+    assert_equal expected, activity.owed
+  end
 
 end
