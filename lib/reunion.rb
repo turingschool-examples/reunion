ea3385563acb7a67c56d33c80b650cd6d899fad9
Tony Robertson <tonyr729@gmail.com>
2018-11-06 09:53:31 -0700

    Add method to satisfy test for calculating reuion total cost

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -9,4 +9,8 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    @activities.inject(0) {|total_cost, activity| total_cost + activity.total_cost}
+  end
 end
